b209e88e38cc9644f0053b23ef618fedede0c31a
Christian Frattini <chrifrat1@gmail.com>
2024-08-13 18:31:28 +0100

    hero section

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,7 +4,7 @@ type ButtonProps = {
   type: "button" | "submit";
   title: string;
   icon?: string;
-  variant: "btn_dark_green";
+  variant: string;
 };
 
 function Button({ type, title, icon, variant }: ButtonProps) {
